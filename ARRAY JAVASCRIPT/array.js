8b5f5df0c0d704dc66158d394aad34cb918a0122
Thiago <thiagooliveira1985@gmail.com>
2020-09-29 22:13:00 -0400

    Initial commit

---
+++ ARRAY JAVASCRIPT/array.js
@@ -0,0 +1,9 @@
+//array
+let tupla = ['thiago',25,'manaus','maça','pera','tomate'];
+console.log(tupla[0]);
+console.log(tupla[1]);
+console.log(tupla[2]);
+console.log(tupla[3]);
+console.log(tupla[4]);
+console.log(tupla[5]);
+console.log(tupla.length)//tamado de propriedades
