432c4289fd8fbb012ab6e992140185efb0417b5f
Thiago <thiagooliveira1985@gmail.com>
2020-09-30 16:08:35 -0400

    .

---
+++ Comparando dois numeros/aula1.js
@@ -0,0 +1,10 @@
+//Comparando dois numeros qual é o maior
+
+let valorMaior = max(1,10);
+console.log(valorMaior);
+
+function max(numero1,numero2){
+  if(numero1 > numero2)
+  return numero1;
+  else return numero2;
+}
